8e39886b5804fbde0cfaaee7bcfd34623be71fc7
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-11-13 10:34:42 +0100

    Create SpringWebApplicationInitializer to register SpringSecurityFilterChain filter

---
+++ src/main/java/com/nnamdi/web/config/SpringWebApplicationInitializer.java
@@ -0,0 +1,9 @@
+package com.nnamdi.web.config;
+
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+
+}
