aee75c960c250898054ef0727218cd6dbac3a7f1
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-11-13 10:47:13 +0100

    Create a UserDetailsServiceImpl which implements custom UserDetailsService and override the loadUserByUsername method.

---
+++ src/main/java/com/nnamdi/web/service/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.nnamdi.web.service;
+
+
+import com.nnamdi.web.model.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername (String username) throws UsernameNotFoundException {
+
+        User user = findUserbyUsername(username);
+
+        org.springframework.security.core.userdetails.User.UserBuilder builder = null;
+        if (user != null) {
+            builder = org.springframework.security.core.userdetails.User.withUsername(username);
+            builder.password(new BCryptPasswordEncoder().encode(user.getPassword()));
+        } else {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return builder.build();
+    }
+
+    private User findUserbyUsername (String username) {
+        if (username.equalsIgnoreCase("admin")) {
+            return new User(username, "123456", "ADMIN");
+        }
+        return null;
+    }
+}
