8aa03dd7c6f58bbc62529735cad04027e85cd5dd
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-11-13 10:42:55 +0100

    Create a springSecurityFilterChain for validating and protecting all urls by creating an @configuration class

--- src/main/java/com/nnamdi/web/config/WebSecurityConfig.java
+++ src/main/java/com/nnamdi/web/config/WebSecurityConfig.java
@@ -1,5 +1,45 @@
-//package com.nnamdi.web.config;
-//
-//public class WebSecurityConfig {
-//}
-////
+package com.nnamdi.web.config;
+
+import com.nnamdi.web.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().hasAnyRole("ADMIN", "USER")
+                .and()
+                .formLogin()
+                .and()
+                .logout().permitAll().logoutSuccessUrl("/login")
+                .and()
+                .csrf().disable();
+    }
+
+}
