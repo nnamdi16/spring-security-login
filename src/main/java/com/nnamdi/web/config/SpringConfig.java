cc8e53d0f53d87c5f324d9f308320e644aaebdae
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-11-14 14:22:26 +0100

    Remove zombie code and limited the EmployeeController to only two routes

--- src/main/java/com/nnamdi/web/config/SpringConfig.java
+++ src/main/java/com/nnamdi/web/config/SpringConfig.java
@@ -3,6 +3,7 @@ package com.nnamdi.web.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -14,15 +15,23 @@ import javax.sql.DataSource;
 @ComponentScan(basePackages = {"com.nnamdi.web"})
 public class SpringConfig {
 
-    @Bean
-    public DataSource getDataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/user_db");
-        dataSource.setUsername("root");
-        dataSource.setPassword("nnamdI2k18#");
-        return dataSource;
-    }
+//    @Bean
+//    public DataSource getDataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/user_db");
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("nnamdI2k18#");
+//        return dataSource;
+//    }
+//
+//
+//    @Bean
+//    public JdbcTemplate jdbcTemplate() {
+//        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+//        jdbcTemplate.setDataSource(getDataSource());
+//        return jdbcTemplate;
+//    }
     /**
      * @Configuration annotation indicates that the class declares one or more @Bean methods which will be processed by the Spring container to generate bean definitions
      * @EnableWebMVc is equivalent to <mvc:annotation-driven/> . It enables support for @Controller, @RestController etc.. annotated classes.
--- src/main/java/com/nnamdi/web/controller/EmployeeController.java
+++ src/main/java/com/nnamdi/web/controller/EmployeeController.java
@@ -6,54 +6,61 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class EmployeeController {
-
-    @Autowired
-    JdbcUserDetailsManager jdbcUserDetailsManager;
-
-    @Autowired
-    PasswordEncoder passwordEncoder;
-
-    @GetMapping("/user")
-    public String showUser() {
-        return "Welcome User !!!!";
-    }
+//
+//    @Autowired
+//    JdbcUserDetailsManager jdbcUserDetailsManager;
+//
+//    @Autowired
+//    PasswordEncoder passwordEncoder;
 
     @GetMapping("/employee")
+    public String showUser() {
+        return "Welcome Employee !!!!";
+    }
+
+    @GetMapping("/manager")
     public String showManager() {
-        return "Welcome Employee !!!";
+        return "Welcome Manager !!!";
     }
 
-    @GetMapping("/user/{username}")
-    public String checkIfUserExists(@PathVariable("username") String username) {
-        boolean flag = jdbcUserDetailsManager.userExists(username);
-        if (flag) {
-            return "\""+username + "\" exist in the Database";
-        } else {
-            return "\""+username + "\" deos not exist in the Database";
-        }
-    }
-
-    @PostMapping("/user/{username}/{password}/{role}")
-    public String createUser(@PathVariable("username") String username, @PathVariable("password")String password, @PathVariable("role") String role) {
-        jdbcUserDetailsManager.createUser(User.withUsername(username).password(passwordEncoder.encode(password)).roles("USER").build());
-        return checkIfUserExists(username);
-    }
-
-    @PutMapping("/user/{username}/{password}/{role}")
-    public String updateUser(@PathVariable("username") String username, @PathVariable("password") String password, @PathVariable("role") String role) {
-        jdbcUserDetailsManager.updateUser(User.withUsername(username).password(passwordEncoder.encode(password)).roles("USER").build());
-        return checkIfUserExists(username);
-    }
-
-    @DeleteMapping("user/{username}")
-    public String deleteUser(@PathVariable("username") String username) {
-        jdbcUserDetailsManager.deleteUser(username);
-        return checkIfUserExists(username);
-    }
+//    @GetMapping("/user/{username}")
+//    public String checkIfUserExists(@PathVariable("username") String username) {
+//        boolean flag = jdbcUserDetailsManager.userExists(username);
+//        if (flag) {
+//            System.out.println(" This is the true ");
+//            return "\""+username + "\" exist in the Database";
+//        } else {
+//            System.out.println("This is not true");
+//            return "\""+username + "\" does not exist in the Database";
+//        }
+//    }
+//
+//    @PostMapping("/user/{username}/{password}/{role}")
+//    public String createUser(@PathVariable("username") String username, @PathVariable("password")String password, @PathVariable("role") String role) {
+//        jdbcUserDetailsManager.createUser(User.withUsername(username).password(passwordEncoder.encode(password)).roles("USER").build());
+//        return checkIfUserExists(username);
+//    }
+//
+//    @PutMapping("/user/{username}/{password}/{role}")
+//    public String updateUser(@PathVariable("username") String username, @PathVariable("password") String password, @PathVariable("role") String role) {
+//        jdbcUserDetailsManager.updateUser(User.withUsername(username).password(passwordEncoder.encode(password)).roles("USER").build());
+//        return checkIfUserExists(username);
+//    }
+//
+//    @DeleteMapping("user/{username}")
+//    public String deleteUser(@PathVariable("username") String username) {
+//        jdbcUserDetailsManager.deleteUser(username);
+//        return checkIfUserExists(username);
+//    }
 
     /**
-     * JdbcUserDetailsManager and Password
+     * JdbcUserDetailsManager and PasswordEncoder classes are autowired.
+     * JdbcUserDetailsManager enables us to create, retrieve, modify nad delete the UserDetails
+     * PasswordEncoder is an implementation of BCryptPasswordEncoder which is used to encode the password.
+     * All request which start with /employee requires USER role and the request which starts with /manager requires MANAGER role.
+     * CheckIfUserExist(), createUser(), updateUser(), deleteUser() methods helps us to make changes to the UserDetails persisted in the database.
      */
 
 }
